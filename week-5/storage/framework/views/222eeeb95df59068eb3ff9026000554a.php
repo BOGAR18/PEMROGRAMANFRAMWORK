6418f71130540a7245df1d32e75debdbbfd8b8ef
BOGAR18 <114753839+BOGAR18@users.noreply.github.com>
2023-05-22 22:43:12 +0700

    Add files via upload
    Praktikum Modul Ke-5

--- week-5/storage/framework/views/222eeeb95df59068eb3ff9026000554a.php
+++ week-5/storage/framework/views/222eeeb95df59068eb3ff9026000554a.php
@@ -30,7 +30,6 @@
             </div>
         </div>
     </nav>
-
     <div class="container mt-4">
         <div class="row mb-0">
             <div class="col-lg-9 col-xl-10">
@@ -44,82 +43,46 @@
         </div>
         <hr>
         <div class="table-responsive border p-3 rounded-3">
-            <table class="table table-bordered table-hover table-striped mb-0 bg-white">
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Age</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Purnama</td>
-                        <td>Anaking</td>
-                        <td>purnama.anaking@gmail.com</td>
-                        <td>20</td>
-                        <td>
-                            <div class="d-flex">
-                                <a href="<?php echo e(route('employees.show', ['employee' => 1])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
-                                <a href="<?php echo e(route('employees.edit', ['employee' => 1])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
 
-                                <div>
-                                    <form action="<?php echo e(route('employees.destroy', ['employee' => 1])); ?>" method="POST">
-                                        <?php echo csrf_field(); ?>
-                                        <?php echo method_field('delete'); ?>
-                                        <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
-                                    </form>
-                                </div>
+    <table class="table table-bordered table-hover table-striped mb-0 bg-white">
+        <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Age</th>
+                <th>Position</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $__currentLoopData = $employees; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $employee): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <tr>
+                    <td><?php echo e($employee->firstname); ?></td>
+                    <td><?php echo e($employee->lastname); ?></td>
+                    <td><?php echo e($employee->email); ?></td>
+                    <td><?php echo e($employee->age); ?></td>
+                    <td><?php echo e($employee->position_name); ?></td>
+                    <td>
+                        <div class="d-flex">
+                            <a href="<?php echo e(route('employees.show', ['employee' => $employee->employee_id])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
+                            <a href="<?php echo e(route('employees.edit', ['employee' => $employee->employee_id])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
+
+                            <div>
+                                <form action="<?php echo e(route('employees.destroy', ['employee' => $employee->employee_id])); ?>" method="POST">
+                                    <?php echo csrf_field(); ?>
+                                    <?php echo method_field('delete'); ?>
+                                    <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
+                                </form>
                             </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Adzanil</td>
-                        <td>Rachmadhi</td>
-                        <td>adzanil.rachmadhi@gmail.com</td>
-                        <td>25</td>
-                        <td>
-                            <div class="d-flex">
-                                <a href="<?php echo e(route('employees.show', ['employee' => 2])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
-                                <a href="<?php echo e(route('employees.edit', ['employee' => 2])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
-
-                                <div>
-                                    <form action="<?php echo e(route('employees.destroy', ['employee' => 2])); ?>" method="POST">
-                                        <?php echo csrf_field(); ?>
-                                        <?php echo method_field('delete'); ?>
-                                        <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
-                                    </form>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Berlian</td>
-                        <td>Rahmy</td>
-                        <td>berlian.rahmy@gmail.com</td>
-                        <td>23</td>
-                        <td>
-                            <div class="d-flex">
-                                <a href="<?php echo e(route('employees.show', ['employee' => 3])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
-                                <a href="<?php echo e(route('employees.edit', ['employee' => 3])); ?>" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
-
-                                <div>
-                                    <form action="<?php echo e(route('employees.destroy', ['employee' => 3])); ?>" method="POST">
-                                        <?php echo csrf_field(); ?>
-                                        <?php echo method_field('delete'); ?>
-                                        <button type="submit" class="btn btn-outline-dark btn-sm me-2"><i class="bi-trash"></i></button>
-                                    </form>
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
+                        </div>
+                    </td>
+                </tr>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </tbody>
+    </table>
+ </div>
+ </div>
     <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
 </body>
 </html>
--- week-5/storage/framework/views/ab5977b0d4616e5dc1df034bd3874a25.php
+++ week-5/storage/framework/views/ab5977b0d4616e5dc1df034bd3874a25.php
@@ -32,12 +32,11 @@
     </nav>
 
     <div class="container-sm mt-5">
-        <form action="<?php echo e(route('employees.store')); ?>" method="POST">
+        <form action="<?php echo e(route('employees.update',['employee' => $employee->employee_id])); ?>" method="POST">
             <?php echo csrf_field(); ?>
+            <?php echo method_field('PUT'); ?>
             <div class="row justify-content-center">
                 <div class="p-5 bg-light rounded-3 border col-xl-6">
-
-                    
 
                     <div class="mb-3 text-center">
                         <i class="bi-person-circle fs-1"></i>
@@ -47,78 +46,109 @@
                     <div class="row">
                         <div class="col-md-6 mb-3">
                             <label for="firstName" class="form-label">First Name</label>
-                            <input class="form-control" type="text" name="firstName" id="firstName" value="<?php echo e(old('firstName')); ?>" placeholder="Enter First Name">
+                            <input class="form-control <?php $__errorArgs = ['firstName'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" type="text" name="firstName" id="firstName" value="<?php echo e($employee -> firstname); ?>" placeholder="Enter First Name">
                             <?php $__errorArgs = ['firstName'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                            <div class="text-danger"><?php echo e($message); ?></div>
-                            <?php unset($message);
+                            <div class="invalid-feedback"><?php echo e($message); ?></div>
+                        <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
                         </div>
+
                         <div class="col-md-6 mb-3">
                             <label for="lastName" class="form-label">Last Name</label>
-                            <input class="form-control" type="text" name="lastName" id="lastName" value="<?php echo e(old('lastName')); ?>" placeholder="Enter Last Name">
+                            <input class="form-control <?php $__errorArgs = ['lastName'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" type="text" name="lastName" id="lastName" value="<?php echo e($employee -> lastname); ?>" placeholder="Enter Last Name">
                             <?php $__errorArgs = ['lastName'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                                <div class="text-danger"><?php echo e($message); ?></div>
-                                <?php unset($message);
+                        <div class="invalid-feedback"><?php echo e($message); ?></div>
+                        <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
                         </div>
+
                         <div class="col-md-6 mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input class="form-control" type="text" name="email" id="email" value="<?php echo e(old('email')); ?>" placeholder="Enter Email">
+                            <input class="form-control <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" type="text" name="email" id="email" value="<?php echo e($employee -> email); ?>" placeholder="Enter email">
                             <?php $__errorArgs = ['email'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                            <div class="text-danger"><?php echo e($message); ?></div>
-                            <?php unset($message);
+                        <p class="text-danger"><?php echo e($message); ?> </p>
+                                <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
                         </div>
+
                         <div class="col-md-6 mb-3">
                             <label for="age" class="form-label">Age</label>
-                            <input class="form-control" type="text" name="age" id="age" value="<?php echo e(old('age')); ?>" placeholder="Enter Age">
+                            <input class="form-control <?php $__errorArgs = ['age'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" type="text" name="age" id="age" value="<?php echo e($employee -> age); ?>" placeholder="Enter age">
                             <?php $__errorArgs = ['age'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                            <div class="text-danger"><?php echo e($message); ?></div>
-                            <?php unset($message);
+                        <p class="text-danger"><?php echo e($message); ?> </p>
+                                <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
                         </div>
-                        <div class="col-md-12 mb-3">
-                            <label for="position" class="form-label">Position</label>
-                            <select name="position" id="position" class="form-select">
-                                <?php $__currentLoopData = $positions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $position): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                                    <option value="<?php echo e($position->id); ?>" <?php echo e(old('position') == $position->id ? 'selected' : ''); ?>><?php echo e($position->code.' - '.$position->name); ?></option>
-                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                            </select>
-                            <?php $__errorArgs = ['position'];
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="position" class="form-label">Position</label>
+                        <select name="position" id="position" class="form-select">
+                            <?php $__currentLoopData = $positions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $position): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <option value="<?php echo e($position->id); ?>" <?php echo e(old('position') == $position->id ? 'selected' : ''); ?>><?php echo e($position->code.' - '.$position->name); ?></option>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </select>
+                        <?php $__errorArgs = ['position'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                                <div class="text-danger"><small><?php echo e($message); ?></small></div>
-                            <?php unset($message);
+                            <div class="text-danger"><small><?php echo e($message); ?></small></div>
+                        <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
-                        </div>
                     </div>
                     <hr>
                     <div class="row">
@@ -137,4 +167,4 @@ unset($__errorArgs, $__bag); ?>
     <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
 </body>
 </html>
-<?php /**PATH C:\week-5\resources\views/employee/create.blade.php ENDPATH**/ ?>
+<?php /**PATH C:\week-5\resources\views/employee/edit.blade.php ENDPATH**/ ?>
